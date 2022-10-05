cf21bef9ded7958facc0811db65d24393ce111fa
huthoinguyn <huthoi99@gmail.com>
2022-10-05 12:01:47 +0700

    #1 Add Home Page

--- site/trang-chinh/index.php
+++ site/trang-chinh/index.php
@@ -5,22 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../content/css/testim.css">
+    <link rel="stylesheet" href="../../content/css/style.css">
+
     <title>T-Coffee</title>
     <style>
-        header {
-            height: 150px;
+        .favourite {
+            min-height: 100vh;
         }
 
-        .row {
-            display: flex;
-            flex-wrap: wrap;
-            margin-left: -15px;
-            margin-right: -15px;
+        section .title {
+            font-size: calc(2vw + 2vh);
+            white-space: nowrap;
+            color: var(--text-color);
+            text-align: center;
+            width: 100%;
+            overflow: hidden;
+        }
+
+        footer {
+            width: 100vw;
+            max-width: 100vw;
             position: relative;
-        }
-
-        .logo {
-            
+            left: calc(-50vw + 50%);
+            min-height: 400px;
+            background-color: var(--bg-color-white);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
         }
     </style>
 </head>
@@ -28,22 +44,65 @@
 <body>
     <div class="container">
         <header>
-            <div class="row">
-                <div class="logo">
-                    <img src="https://coffeeluck.themerex.net/wp-content/uploads/2016/08/logo.png" alt="">
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Coffee</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Contact</a></li>
-                        <li><a href="">Hoi dap</a></li>
-                    </ul>
-                </nav>
+            <div class="logo">
+                <img src="https://coffeeluck.themerex.net/wp-content/uploads/2016/08/logo.png" alt="">
             </div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Coffee</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">Hoi dap</a></li>
+                </ul>
+            </nav>
         </header>
+        <div class="slider">
+            <div class="slider-img">
+                <img src="https://coffeeluck.themerex.net/wp-content/uploads/2016/08/slide1.jpg" alt="">
+            </div>
+            <div class="slider-content">
+                <h3>Coffee with our story</h3>
+                <p>visit our the best coffee shop</p>
+            </div>
+        </div>
+        <div class="content">
+            <section class="favourite">
+                <div class="row">
+                    <h3 class="title">Top 10</h3>
+                </div>
+            </section>
+            <section class="testim">
+                <div class="testim-wrap">
+                    <div class="testim-left">
+                        <i class="fa-solid fa-angle-left"></i>
+                    </div>
+                    <div class="testim-content">
+
+                    </div>
+                    <ul class="testim-dots">
+                        <li class="dot active"></li>
+                        <li class="dot"></li>
+                        <li class="dot"></li>
+                        <li class="dot"></li>
+                        <!-- <li class="dot"></li>
+                <li class="dot"></li> -->
+                    </ul>
+                    <div class="testim-right">
+                        <i class="fa-solid fa-angle-right"></i>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <footer>
+            This is footer
+        </footer>
     </div>
+    <script src="../../content/js/script.js"></script>
+    <script src="../../content/js/testim.js"></script>
+    <script>
+
+    </script>
 
 </body>
 
