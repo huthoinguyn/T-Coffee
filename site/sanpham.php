9ef2bd28f05fb999b43e43d4075b11536ab0aa70
TanTaiDayyy <108246926+TanTaiDayyy@users.noreply.github.com>
2022-10-06 13:23:21 +0700

    plepleee

--- site/sanpham.php
+++ site/sanpham.php
@@ -5,6 +5,7 @@
   <title>Product </title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined|Material+Icons" rel="stylesheet" />
 <link rel="stylesheet" href="../content/css/sanpham.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <main>
@@ -17,10 +18,11 @@
 					</div>
 					<div class="product-content">
 						<div class="product-info">
-							<h2 class="product-title">Cosmonaut</h2>
+							<h2 class="product-title">Thoicapuchino</h2>
 							<p class="product-price">$ 10</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 
@@ -33,7 +35,8 @@
 							<h2 class="product-title">Monster Milkshake</h2>
 							<p class="product-price">$ 9</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 				<a class="product" href="#">
@@ -45,7 +48,8 @@
 							<h2 class="product-title">Juicy Banana</h2>
 							<p class="product-price">$ 9</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 			</div>
@@ -59,7 +63,8 @@
 							<h2 class="product-title">Palmistry</h2>
 							<p class="product-price">$ 9</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 				<a class="product" href="#">
@@ -71,7 +76,8 @@
 							<h2 class="product-title">Fish With Gas Mask</h2>
 							<p class="product-price">$ 12</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 				<a class="product" href="#">
@@ -83,7 +89,8 @@
 							<h2 class="product-title">Gangster</h2>
 							<p class="product-price">$ 5</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 			</div>
@@ -97,7 +104,8 @@
 							<h2 class="product-title">Adventure in a Bottle</h2>
 							<p class="product-price">$ 15</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 				<a class="product" href="#">
@@ -109,7 +117,8 @@
 							<h2 class="product-title">Smoking Ain't Cool</h2>
 							<p class="product-price">$ 5</p>
 						</div>
-						<button class="product-action"><i class="material-icons-outlined">favorite_border</i></button>
+						<button class="product-action"><i class="fa-solid fa-plus"></i></button>
+						<!-- <button class="product-action"><i class="material-icons-outlined">favorite_border</i></button> -->
 					</div>
 				</a>
 			</div>
